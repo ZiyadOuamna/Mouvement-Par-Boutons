9c9db84670ca77c001bdc61b5009f7a5d1b9293d
Ziyad Ouamna <117644947+ZiyadOuamna@users.noreply.github.com>
2023-05-25 10:26:23 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let toRight = document.getElementById('right');
+let toLeft  = document.getElementById('left');
+var division = document.getElementById('divChild');
+function marginToRight(){
+        division.style.transform +=  "translateX(15px)";
+ 
+    
+}
+function marginToLeft(){
+    division.style.transform += "translateX(-15px)";
+}
